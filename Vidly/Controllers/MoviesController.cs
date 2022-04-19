26c3a3176a1af806d9581d038439af979ce220ed
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-04-19 11:30:23 +0530

    ASP .NET Fundamentals Done

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,23 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie()
+            var movie = new List<Movie>
             {
-                Name = "Shrek"
+                new Movie{Name = "Shrek" }
             };
-            return View(movie);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer 1"},
+                new Customer{Name="Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
         // /movies/edit?id=1
@@ -29,28 +47,22 @@ namespace Vidly.Controllers
             return Content("id = " + id);
         }
 
-        // /movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content($"pageIndex = {pageIndex}, sortBy = {sortBy}");
-        }
-
         // movies/released/{year}/{month}
 
         [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year,int month)
+        public ActionResult ByReleaseDate(int year, int month)
         {
             return Content($"{year}/{month}");
+        }
+
+        // /movies
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
+        {
+
+            var movies = GetMovies();
+
+            return View(movies);
         }
     }
 }
