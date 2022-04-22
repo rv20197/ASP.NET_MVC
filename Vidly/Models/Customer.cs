0d84d1281f405539ace5ac342acc76cd89c82516
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-04-22 08:26:23 +0530

    Custom Validation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -20,6 +20,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? CustomerBirthDate { get; set; }
     }
 }
---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
+            if (customer.CustomerBirthDate == null)
+            {
+                return new ValidationResult("Date Of Birth is Required");
+            }
+
+            var age = DateTime.Today.Year - customer.CustomerBirthDate.Value.Year;
+
+            return (age > 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Customer should be atleast 18years old to go on a membership");
+        }
+    }
+}
