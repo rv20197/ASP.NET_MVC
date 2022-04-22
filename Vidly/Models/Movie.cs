c1da447702e64e94fb912e56f0a1e4d48ce4f4e9
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-04-22 10:15:17 +0530

    Added Validation to the Movie Module

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -20,10 +20,12 @@ namespace Vidly.Models
         public DateTime DateAdded { get; set; }
 
         [Display(Name = "Release Date")]
+        [Required]
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "Number in Stock")]
         [Range(1, 20)]
+        [Required]
         public byte NumberInStock { get; set; }
     }
 }
