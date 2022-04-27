8557c2f38aedaed959e69c76957422210f063851
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-04-27 07:44:59 +0530

    Output Cache

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -10,23 +10,10 @@ namespace Vidly.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        [OutputCache(Duration = 50, Location = OutputCacheLocation.Server,VaryByParam = "*")]
+        //[OutputCache(Duration = 50, Location = OutputCacheLocation.Server,VaryByParam = "*")]
+        [OutputCache(Duration = 0,VaryByParam = "*",NoStore = true)]
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
